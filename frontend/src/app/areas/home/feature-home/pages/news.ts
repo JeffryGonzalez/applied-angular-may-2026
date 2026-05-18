5f8d73436b5ca9f79ab2719b9cadd069ae5ef2bb
Jeffry Gonzalez <jeff@hypertheory.com>
2026-05-18 12:41:21 -0400

    showed msw-lens

--- frontend/src/app/areas/home/feature-home/pages/news.ts
+++ frontend/src/app/areas/home/feature-home/pages/news.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { PageHeader } from '../../../shared/ui-page-header/page-header';
 import { NewsList } from '../ui/news-list';
 import { NewsItem } from '../data/types';
+import { httpResource } from '@angular/common/http';
 
 @Component({
   selector: 'app-home-news',
@@ -9,24 +10,13 @@ import { NewsItem } from '../data/types';
   template: `
     <app-page-header title="Angular News" description="Recent News About Angular" />
     <div class="prose max-w-none">
-      <app-home-news-list [news]="newList" />
+      @if (newsResource.hasValue()) {
+        <app-home-news-list [news]="newsResource.value()" />
+      }
     </div>
   `,
   styles: ``,
 })
 export class NewsPage {
-  newList: NewsItem[] = [
-    {
-      id: '1',
-      title: 'Angular 22 Coming Soon',
-      body: 'Angular is going to be released super soon!',
-      published: '2026-05-18T15:48:12.499Z',
-    },
-    {
-      id: '2',
-      title: 'NGRX Signal Store Now Has Events',
-      body: 'Signal Store has events, which makes it a replacement for NGRX store',
-      published: '2026-05-18T15:48:12.499Z',
-    },
-  ];
+  newsResource = httpResource<NewsItem[]>(() => 'https://news.hypertheory.com/angular');
 }
