43baf969e95b1a9a897f00aeb6a8fcd0443988b0
Jeffry Gonzalez <jeff@hypertheory.com>
2026-05-20 10:32:52 -0400

    before software

--- frontend/src/app/areas/demos/data-counter/store.ts
+++ frontend/src/app/areas/demos/data-counter/store.ts
@@ -1,11 +1,13 @@
-import { Injectable, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { signal } from '@angular/core';
 import { patchState, signalStore, withMethods, withState } from '@ngrx/signals';
 import { withLogging } from '../../shared/util-logging/store-logging-feature';
 
-@Injectable({ providedIn: 'root' })
+// @Service() // makes it provided in root, you DO NOT HAVE TO DO THIS for a service. Oh, it will give you an error if you do constructor injection.
 export class CounterStore {
   #current = signal(0);
 
+  constructor(private http: HttpClient) {}
   get current() {
     return this.#current.asReadonly();
   }
