410b19655fc03ab47f7a744dd7fcdad390cd2cb9
Val Neculaescu <tinthetomato@gmail.com>
2026-05-21 19:49:43 +0000

    Second commit message

--- frontend/src/app/areas/todos/feature-home/todos.routes.ts
+++ frontend/src/app/areas/todos/feature-home/todos.routes.ts
@@ -1,13 +1,19 @@
 import { Routes } from '@angular/router';
 import { Home } from './home';
+import { todosStore } from '../data/store';
 
 export const todosRoutes: Routes = [
   {
     path: '',
     component: Home,
+    providers: [todosStore],
     data: { area: { label: 'Todos' } },
     children: [
-      // list page goes here
+      {
+  path: '',
+  loadComponent: () => import('./pages/list').then((m) => m.ListPage),
+  data: { nav: { label: 'List' } },
+},
     ],
   },
 ];
